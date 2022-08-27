6f39e723097a77b3d023ec255f30373ee21a5995
oiesc <45150141+oiesc@users.noreply.github.com>
2022-08-27 14:43:32 -0300

    start project

---
+++ src/main/java/com/example/trabalhofullstackauth/TrabalhoFullstackAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalhofullstackauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFullstackAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoFullstackAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trabalhofullstackauth/TrabalhoFullstackAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhofullstackauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFullstackAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
