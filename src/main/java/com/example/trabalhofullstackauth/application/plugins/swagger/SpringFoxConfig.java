46f9cce1c615b0a50cb7f0b2a300ef3f60e61c86
oiesc <45150141+oiesc@users.noreply.github.com>
2022-09-11 18:40:38 -0300

    config springfox

--- src/main/java/com/example/trabalhofullstackauth/application/plugins/swagger/SpringFoxConfig.java
+++ src/main/java/com/example/trabalhofullstackauth/application/plugins/swagger/SpringFoxConfig.java
@@ -34,7 +34,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("api.html").addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
