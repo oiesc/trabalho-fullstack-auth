9e3042d9923a24f3b9a732957e7155c2d13afd53
oiesc <45150141+oiesc@users.noreply.github.com>
2022-09-11 18:30:07 -0300

    config springfox

--- src/main/java/com/example/trabalhofullstackauth/application/plugins/swagger/SpringFoxConfig.java
+++ src/main/java/com/example/trabalhofullstackauth/application/plugins/swagger/SpringFoxConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,10 +19,17 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.trabalhofullstackauth"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("REST API")
+                .description("Servicesx")
                 .build();
     }
 
